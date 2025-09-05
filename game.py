298f2b020319b2a87182a9a0644780f532167c14
jek710 <jek710@mail.usask.ca>
2025-09-05 13:02:19 -0600

    Mouse interaction optimized
    * Time efficiency improved by calculating
      the button index rather than iterating
      through every button.

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import random
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
@@ -21,6 +21,7 @@ plt.ylim(-1.2 * 10/n - 2, 1.5 * 10/n)
 lamp_states = np.random.randint(0, 2, n)   # 1 is on, 0 is off
 lamps = []
 buttons = []
+hovered: patches.Rectangle = None   # saves the instance of the button being hovered over
 pressed: patches.Rectangle = None   # saves the instance of the button being pressed
 for i in range(n):
     color_lamp = color_lamp_off if lamp_states[i] == 0 else color_lamp_on
@@ -29,25 +30,31 @@ for i in range(n):
     ax.add_patch(lamps[i])
     ax.add_patch(buttons[i])
 
+def find_button(x: float, y: float) -> patches.Rectangle | None:
+    if x < 0 or x > 10 or y < -2 - 10/n or y > -2:
+        # not on a button
+        return None
+    return buttons[math.floor(n * x / 10)]
+
 def on_move(event: MouseEvent):
+    global hovered
     if event.inaxes != ax:
         return
-    for b in buttons:
-        if isinstance(b, patches.Rectangle):
-            inside, _ = b.contains(event)
-            if inside and b is not pressed:
-                b.set_facecolor(color_switch_hover)
-            else:
-                b.set_facecolor(color_switch_idle)
+    if hovered:
+        hovered.set_facecolor(color_switch_idle)
+        hovered = None
+    b = find_button(event.xdata, event.ydata)
+    if isinstance(b, patches.Rectangle) and b is not pressed:
+        b.set_facecolor(color_switch_hover)
+        hovered = b
     fig.canvas.draw_idle()
 
 def on_press(event: MouseEvent):
     global pressed
-    for b in buttons:
-        if b.contains(event)[0]:
-            pressed = b
-            b.set_facecolor(color_switch_pressed)
-            break
+    b = find_button(event.xdata, event.ydata)
+    if isinstance(b, patches.Rectangle):
+        pressed = b
+        b.set_facecolor(color_switch_pressed)
     fig.canvas.draw_idle()
 
 def on_release(event: MouseEvent):
